83cbee2beaaee1c4a0c367e5eb089b549ba9498f
kauanm14 <00001119566344sp@al.educacao.sp.gov.br>
2024-11-18 13:48:26 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showString("" + (receivedNumber))
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("ola mundo")
+})
+radio.setGroup(1)
